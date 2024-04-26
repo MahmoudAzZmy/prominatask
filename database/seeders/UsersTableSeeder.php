0d33d82ff5d66a2ab48845545ccfadaa523debe4
MahmoudAzZmy <mahmoud667j@gmail.com>
2024-04-26 03:25:18 +0300

    first push

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+                'email_verified_at' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
